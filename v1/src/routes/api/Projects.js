e8560899edd4cfb662d0abe69acf09ffc55b26f2
aghabalaguluzade <aghabalaguluzade@gmail.com>
2024-10-01 23:08:49 +0400

    implement validation with Joi

--- v1/src/routes/api/Projects.js
+++ v1/src/routes/api/Projects.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import { index, store } from '../../controllers/Projects.js';
+import { storeValidation } from '../../validations/Projects.js';
+import { validate } from '../../middlewares/validate.js';
 
 const router = express.Router();
 
 router.get('/', index);
-router.post('/', store);
+router
+   .route('/')
+   .post(validate(storeValidation), store);
 
 export default router;
---
+++ v1/src/validations/Projects.js
@@ -0,0 +1,9 @@
+import Joi from "joi";
+
+const storeValidation = Joi.object({
+   name: Joi.string().required().min(5)
+});
+
+export {
+   storeValidation
+}
